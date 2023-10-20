fc3c1743145bfd3a23228f4f8bd56d3dd91ba5ec
joseluas888 <116414240+joseluas888@users.noreply.github.com>
2023-10-20 13:58:46 -0600

    Add files via upload

---
+++ Caritas_Cuervos/Caritas_Cuervos/Holaaaaa.swift
@@ -0,0 +1,20 @@
+//
+//  Holaaaaa.swift
+//  Caritas_Cuervos
+//
+//  Created by Alumno on 20/10/23.
+//
+
+import SwiftUI
+
+struct Holaaaaa: View {
+    var body: some View {
+        Text("Vamooos cuervos")
+    }
+}
+
+struct Holaaaaa_Previews: PreviewProvider {
+    static var previews: some View {
+        Holaaaaa()
+    }
+}
