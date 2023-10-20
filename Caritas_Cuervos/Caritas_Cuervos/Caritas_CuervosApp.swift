fe24d842555b17927a20a99a45bad478f319d0b5
nalle23 <80711958+nalle23@users.noreply.github.com>
2023-10-20 12:54:18 -0600

    ya jala el login

---
+++ Caritas_Cuervos/Caritas_Cuervos/Caritas_CuervosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Caritas_CuervosApp.swift
+//  Caritas_Cuervos
+//
+//  Created by Alumno on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Caritas_CuervosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Caritas_Cuervos/Caritas_Cuervos/PruebasAPI.swift
@@ -0,0 +1,32 @@
+//
+//  PruebasAPI.swift
+//  Caritas_Cuervos
+//
+//  Created by Alumno on 18/10/23.
+//
+/*
+import SwiftUI
+
+
+struct PruebasAPI: View {
+    @State private var listaUsuario: Array<Usuario> = []
+    
+    var body: some View {
+        VStack{
+            Text("Pruebas de API")
+            Button("Llamar API"){
+                listaUsuario = CallAPIUsuario()
+            }.buttonStyle(.borderedProminent)
+            List(listaUsuario){
+                itemUsuaruio in Text("Id: \(itemUsuaruio.id) - Usuario: \(itemUsuaruio.Matricula) - Contraseña \(itemUsuaruio.password)")
+            }
+        }
+    }
+}
+
+struct PruebasAPI_Previews: PreviewProvider {
+    static var previews: some View {
+        PruebasAPI()
+    }
+}
+*/
