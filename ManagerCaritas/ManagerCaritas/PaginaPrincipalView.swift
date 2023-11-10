3f213336f1543d310647847bfa5619f60ece4df6
joseluas888 <116414240+joseluas888@users.noreply.github.com>
2023-11-10 13:14:21 -0600

    Resumen del dia

--- ManagerCaritas/ManagerCaritas/PaginaPrincipalView.swift
+++ ManagerCaritas/ManagerCaritas/PaginaPrincipalView.swift
@@ -5,11 +5,39 @@
 //  Created by Alumno on 05/11/23.
 //
 
+
 import SwiftUI
 
 struct PaginaPrincipalView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            TabView{
+                Text("")
+                    .tabItem {Label("Todos", systemImage: "chart.bar.fill")}
+                Text("")
+                    .tabItem {Label("Cobrados", systemImage: "dollarsign.circle.fill")}
+                Text("")
+                    .tabItem {Label("Comentarios", systemImage: "text.bubble")}
+                Text("")
+                    .tabItem {Label("No Cobrados", systemImage: "exclamationmark.triangle")}
+                
+            }
+            Spacer()
+
+            VStack{
+                Text ("Resumen del día")
+                    .padding()
+                // Aquí es donde iría el contenido principal de tu página.
+            }.frame(height: 730)
+            
+            TabView{
+                Text("")
+                    .tabItem {Label("Todos", systemImage: "chart.bar.fill")}
+                Text("")
+                    .tabItem {Label("Cobrados", systemImage: "dollarsign.circle.fill")}
+            }.padding(.horizontal, 80.0)
+            
+        }
     }
 }
 
---
+++ ManagerCaritas/ManagerCaritas/RecibosComentadosView.swift
@@ -0,0 +1,16 @@
+//
+//  RecibosComentadosView.swift
+//  ManagerCaritas
+//
+//  Created by Alumno on 10/11/23.
+//
+
+import SwiftUI
+
+struct RecibosComentadosView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
