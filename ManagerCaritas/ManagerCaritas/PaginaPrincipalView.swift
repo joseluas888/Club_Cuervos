3c9e25d22cf21559492a00735addd71c3e04efa4
A01174206 <132175693+A01174206@users.noreply.github.com>
2023-11-24 15:06:41 -0600

    Add files via upload

--- ManagerCaritas/ManagerCaritas/PaginaPrincipalView.swift
+++ ManagerCaritas/ManagerCaritas/PaginaPrincipalView.swift
@@ -15,33 +15,75 @@ var naranja:Color = Color(red:1, green:0.4627450980392157, blue:0.17254901960784
 var verde:Color = Color(red:0.792156862745098, green:0.8666666666666667, blue:0.8274509803921568)
 
 struct PaginaPrincipalView: View {
+    @State private var selectedTab: Tab = .recolectores
     @State private var showingLoginView = false
+    @State private var showingLogoutConfirmation = false
+
+    enum Tab {
+        case recolectores
+        case dashboards
+    }
 
     var body: some View {
         NavigationView {
             NavigationStack {
                 VStack {
-                    TabView {
+                    TabView(selection: $selectedTab) {
                         ListaRecolectoresView()
-                            .tabItem { Label("Recolectores", systemImage: "bicycle") }
+                            .tabItem {
+                                Label("Recolectores", systemImage: "bicycle")
+                                    .tag(Tab.recolectores)
+                            }
                         DashboardsView()
-                            .tabItem { Label("Dashboards", systemImage: "chart.bar.fill") }
+                            .tabItem {
+                                Label("Dashboards", systemImage: "chart.bar.fill")
+                                    .tag(Tab.dashboards)
+                            }
+                    }
+                    .onChange(of: selectedTab) { newTab in
+                        // Oculta el botón cuando cambias a la pestaña Dashboards
+                        if newTab == .dashboards {
+                            showingLoginView = false
+                        }
                     }
                 }
                 .navigationBarBackButtonHidden(true)
-                .navigationBarItems(
-                    trailing: Button(action: {
-                        showingLoginView.toggle()
-                    }) {
-                        Image(systemName: "rectangle.portrait.and.arrow.forward")
-                            .resizable()
-                            .frame(width: 24, height: 24)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        if selectedTab == .recolectores {
+                            Button(action: {
+                                showingLogoutConfirmation = true
+                            }) {
+                                Image(systemName: "arrow.left")
+                                    .resizable()
+                                    .frame(width: 24, height: 24)
+                                    .foregroundColor(Color.black)
+                            }
+                            .alert(isPresented: $showingLogoutConfirmation) {
+                                Alert(
+                                    title: Text("Cerrar Sesión"),
+                                    message: Text("¿Estás seguro de que quieres cerrar sesión?"),
+                                    primaryButton: .default(Text("Sí")) {
+                                        showingLoginView.toggle()
+                                    },
+                                    secondaryButton: .cancel(Text("No"))
+                                )
+                            }
+                        }
                     }
-                    .sheet(isPresented: $showingLoginView) {
-                        LoginView()
+                }
+                .background(
+                    NavigationLink(destination: LoginView().navigationBarBackButtonHidden(true), isActive: $showingLoginView) {
+                        EmptyView()
                     }
+                    .isDetailLink(false)
+                    .hidden()
                 )
             }
+        }
+        .onAppear {
+            // Cambiar el color de la flecha de retroceso a negro
+            UINavigationBar.appearance().tintColor = .black
         }
     }
 }
